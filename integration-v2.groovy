f86c09c3d7284c2f55a53ab53ef62b82f30c3855
Kavinda Rajapakse <kavindasr@gmail.com>
2025-10-06 14:47:12 +0530

    Remove duplicate shebang line from integration-v2.groovy

--- integration-v2.groovy
+++ integration-v2.groovy
@@ -1,3 +1,4 @@
+#!groovy
 /*
  * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
  *
@@ -15,8 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-#!groovy
 
 import groovy.json.JsonSlurperClassic 
 
