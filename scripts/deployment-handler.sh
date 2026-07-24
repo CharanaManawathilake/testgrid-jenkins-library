ad010ca20d534364b99c8943092c95ceb2489b89
CharanaManawathilake <124888275+CharanaManawathilake@users.noreply.github.com>
2026-07-24 15:52:56 +0530

    Comment out integration deployment logic
    Comment out the conditional deployment logic for integration tests and add a log for the region.

--- scripts/deployment-handler.sh
+++ scripts/deployment-handler.sh
@@ -67,17 +67,18 @@ function changeCommonLogPath(){
 function cloudformationDeployment(){
     log_info "Executing product specific deployment..."
     log_info "Running ${product} deployment.."
-    if [[ ${testType} == "intg"  ]];
-    then
-        if [[ ${product} == "wso2am" ]];
-        then
-            bash ${currentScript}/apim/intg/intg-deploy.sh ${deploymentName} ${cloudformationFileLocations[@]}
-        else
-            bash ${currentScript}/${product}/intg/intg-deploy.sh ${deploymentName} ${cloudformationFileLocations[@]}
-        fi
-    else
-        bash ${currentScript}/${product}/deploy.sh ${deploymentName} ${cloudformationFileLocations[@]}
-    fi
+    log_info "REGION IS : ${region}"
+    # if [[ ${testType} == "intg"  ]];
+    # then
+    #     if [[ ${product} == "wso2am" ]];
+    #     then
+    #         bash ${currentScript}/apim/intg/intg-deploy.sh ${deploymentName} ${cloudformationFileLocations[@]}
+    #     else
+    #         bash ${currentScript}/${product}/intg/intg-deploy.sh ${deploymentName} ${cloudformationFileLocations[@]}
+    #     fi
+    # else
+    #     bash ${currentScript}/${product}/deploy.sh ${deploymentName} ${cloudformationFileLocations[@]}
+    # fi
     if [[ $? != 0 ]];
     then
         # If deployment fails the handler should also fail
