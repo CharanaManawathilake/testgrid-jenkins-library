9fbbdc326d7429c3732f5da81ec096d478c0b9a7
CharanaManawathilake <124888275+CharanaManawathilake@users.noreply.github.com>
2026-08-07 11:24:28 +0530

    Change agent label from 'test-pipeline-agent' to 'dummy-node'

--- main.groovy
+++ main.groovy
@@ -26,7 +26,7 @@ def s3BuildLogPath = ""
 def s3PathConstructor = ""
 
 pipeline {
-agent {label 'test-pipeline-agent'}
+agent {label 'dummy-node'}
 stages {
     stage('Clone CFN repo') {
         steps {
